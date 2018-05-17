ee7b65fb7222ede8670b28a2b1f37a92ea14c3a2
Mauro Cicolella <mcicolella@libero.it>
2018-05-17 17:12:17 +0200

    Add hystrix-circuit-breaker project

---
+++ hystrix-circuit-breaker/src/test/java/net/emmecilab/hystrixcircuitbreaker/Demo/Hystrix/Circuit/Breaker/DemoHystrixCircuitBreakerApplicationTests.java
@@ -0,0 +1,16 @@
+package net.emmecilab.hystrixcircuitbreaker.Demo.Hystrix.Circuit.Breaker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoHystrixCircuitBreakerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
