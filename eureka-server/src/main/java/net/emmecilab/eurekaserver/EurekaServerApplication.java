e10965004a40e580b1677a32fc4e5f08778cbcfa
Mauro Cicolella <mcicolella@libero.it>
2018-06-01 11:51:56 +0200

    Rename eureka-naming-server project to eureka-server

--- eureka-server/src/main/java/net/emmecilab/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/net/emmecilab/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package net.emmecilab.eureka.naming.server;
+package net.emmecilab.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaNamingServerApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaNamingServerApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
